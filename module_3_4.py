06bdc7d9bdf2317d0d711fa3967ab323543f6f47
maximka1366 <maximka66@mail.ru>
2024-07-16 16:40:49 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,18 @@
+def single_root_words(root_word, *other_words):
+    same_words = list()
+    for i in other_words:
+        if root_word.upper() in i.upper():
+            same_words.append(i)
+    for i in other_words:
+        if  i.upper() in  root_word.upper():
+            if  same_words.count(i) :
+                continue
+            else:
+                same_words.append(i)
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
